51f2ac6ff33eb25e5889d34815f7f7dffc42a5b1
CyberAMS <38402937+CyberAMS@users.noreply.github.com>
2018-12-18 08:31:16 -0800

    Progress

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "json.hpp"
 #include <math.h>
 #include "particle_filter.h"
+#include "helper_functions.h"
 
 using namespace std;
 
@@ -32,6 +33,7 @@ std::string hasData(std::string s) {
 // define constants
 const bool bFILEOUTPUT = false;
 const bool bDISPLAY = true;
+const bool bTEST = true;
 
 // define file for redirecting standard output and append
 ofstream out("out.txt", fstream::app);
@@ -39,206 +41,258 @@ streambuf *coutbuf = cout.rdbuf(); // save screen object
 
 int main()
 {
-	
-	uWS::Hub h;
-	
-	//Set up parameters here
-	double delta_t = 0.1; // Time elapsed between measurements [sec]
-	double sensor_range = 50; // Sensor range [m]
-	double sigma_pos [3] = {0.3, 0.3, 0.01}; // GPS measurement uncertainty [x [m], y [m], theta [rad]]
-	double sigma_landmark [2] = {0.3, 0.3}; // Landmark measurement uncertainty [x [m], y [m]]
-	
-	// Read map data
-	Map map;
-	if (!read_map_data("../data/map_data.txt", map)) {
+	// bypass connection to simulator and use debugging environment instead
+	if (bTEST) {
 		
-		cout << "Error: Could not open map file" << endl;
+		// define constants
+		const unsigned int num_debug_steps = 5;
+		const double INIT_x = 0;
+		const double INIT_y = 2.5;
+		const double INIT_theta = 0 * M_PI / 1;
 		
-		return -1;
+		// define variables
+		unsigned int current_debug_step = 0;
+		double sense_x = 0:
+		double sense_y = 0;
+		double sense_theta = 0;
+		double sigma_pos [3] = {0.3, 0.3, 0.01};
+		double delta_t = 0.1;
+		double sensor_range = 50; // Sensor range [m]
+		double sigma_landmark [2] = {0.3, 0.3}; // Landmark measurement uncertainty [x [m], y [m]]		
+		double previous_velocity = 0;
+		double previous_yawrate = 0;
 		
-	}
-	
-	// Create particle filter
-	ParticleFilter pf;
-	
-	h.onMessage([&pf,&map,&delta_t,&sensor_range,&sigma_pos,&sigma_landmark](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
-		// "42" at the start of the message means there's a websocket message event.
-		// The 4 signifies a websocket message
-		// The 2 signifies a websocket event
-		
-		if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+		// Create particle filter
+		ParticleFilter pf;
 			
-			auto s = hasData(std::string(data));
+		for (current_debug_step = 0; current_debug_step < num_debug_steps; current_debug_step++) {
 			
-			if (s != "") {
+			// init or predict
+			if (!pf.initialized()) {
 				
-				auto j = json::parse(s);
-				std::string event = j[0].get<std::string>();
+				sense_x = INIT_x;
+				sense_y = INIT_y;
+				sense_theta = INIT_theta;
+								
+				pf.init(sense_x, sense_y, sense_theta, sigma_pos);
 				
-				if (event == "telemetry") {
-					// j[1] is the data JSON object
-					
-					// redirect standard output to file if necessary
-					if (bFILEOUTPUT) {
-						cout.rdbuf(out.rdbuf());
-					}
-					
-					// display message if required
-					if (bDISPLAY) {
-						cout << "= = = = = = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
-						cout << "MAIN: onMessage - Start" << endl;
-					}
-					
-					if (!pf.initialized()) {
-						// Sense noisy position data from the simulator
-						
-						double sense_x = std::stod(j[1]["sense_x"].get<std::string>());
-						double sense_y = std::stod(j[1]["sense_y"].get<std::string>());
-						double sense_theta = std::stod(j[1]["sense_theta"].get<std::string>());
-						
-						pf.init(sense_x, sense_y, sense_theta, sigma_pos);
-						
-					}
-					else {
-						// Predict the vehicle's next state from previous (noiseless control) data.
-						
-						double previous_velocity = std::stod(j[1]["previous_velocity"].get<std::string>());
-						double previous_yawrate = std::stod(j[1]["previous_yawrate"].get<std::string>());
-						
-						pf.prediction(delta_t, sigma_pos, previous_velocity, previous_yawrate);
-						
-					}
-					
-					// receive noisy observation data from the simulator
-					// sense_observations in JSON format [{obs_x,obs_y},{obs_x,obs_y},...{obs_x,obs_y}]
-					vector<LandmarkObs> noisy_observations;
-					string sense_observations_x = j[1]["sense_observations_x"];
-					string sense_observations_y = j[1]["sense_observations_y"];
-					
-					std::vector<float> x_sense;
-					std::istringstream iss_x(sense_observations_x);
-					std::copy(std::istream_iterator<float>(iss_x),
-					std::istream_iterator<float>(),
-					std::back_inserter(x_sense));
-					
-					std::vector<float> y_sense;
-					std::istringstream iss_y(sense_observations_y);
-					std::copy(std::istream_iterator<float>(iss_y),
-					std::istream_iterator<float>(),
-					std::back_inserter(y_sense));
-					
-					for(unsigned int i = 0; i < x_sense.size(); i++) {
-						
-						LandmarkObs obs;
-						obs.x = x_sense[i];
-						obs.y = y_sense[i];
-						noisy_observations.push_back(obs);
-						
-					}
-					
-					// Update the weights and resample
-					pf.updateWeights(sensor_range, sigma_landmark, noisy_observations, map);
-					pf.resample();
-					
-					// Calculate and output the average weighted error of the particle filter over all time steps so far.
-					vector<Particle> particles = pf.particles;
-					int num_particles = particles.size();
-					double highest_weight = -1.0;
-					Particle best_particle;
-					double weight_sum = 0.0;
-					for (int i = 0; i < num_particles; ++i) {
-						
-						if (particles[i].weight > highest_weight) {
-							
-							highest_weight = particles[i].weight;
-							best_particle = particles[i];
-							
-						}
-						
-						weight_sum += particles[i].weight;
-						
-					}
-					cout << "highest w " << highest_weight << endl;
-					cout << "average w " << weight_sum/num_particles << endl;
-					
-					json msgJson;
-					msgJson["best_particle_x"] = best_particle.x;
-					msgJson["best_particle_y"] = best_particle.y;
-					msgJson["best_particle_theta"] = best_particle.theta;
-					
-					//Optional message data used for debugging particle's sensing and associations
-					msgJson["best_particle_associations"] = pf.getAssociations(best_particle);
-					msgJson["best_particle_sense_x"] = pf.getSenseX(best_particle);
-					msgJson["best_particle_sense_y"] = pf.getSenseY(best_particle);
-					
-					auto msg = "42[\"best_particle\"," + msgJson.dump() + "]";
-					// std::cout << msg << std::endl;
-					ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-					
-					// display message if required
-					if (bDISPLAY) {
-						cout << "  msg: " << endl << msg << endl;
-						cout << "--- MAIN: onMessage - End" << endl;
-						cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
-					}
-					
-					// set standard output to screen if necessary
-					if (bFILEOUTPUT) {
-						cout.rdbuf(coutbuf);
-					}
-					
-				}
+			}
+			else {
+				// Predict the vehicle's next state from previous (noiseless control) data.
 				
-			} else {
+				double previous_velocity = std::stod(j[1]["previous_velocity"].get<std::string>());
+				double previous_yawrate = std::stod(j[1]["previous_yawrate"].get<std::string>());
 				
-				std::string msg = "42[\"manual\",{}]";
-				ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+				pf.prediction(delta_t, sigma_pos, previous_velocity, previous_yawrate);
 				
 			}
 			
 		}
 		
-	});
-	
-	// We don't need this since we're not using HTTP but if it's removed the program
-	// doesn't compile :-(
-	h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
-		
-		const std::string s = "<h1>Hello world!</h1>";
-		
-		if (req.getUrl().valueLength == 1) {
-			
-			res->end(s.data(), s.length());
-			
-		}
-		else {
-			
-			// i guess this should be done more gracefully?
-			res->end(nullptr, 0);
-			
-		}
-		
-	});
-	
-	h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-		std::cout << "Connected!!!" << std::endl;
-	});
-	
-	h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-		ws.close();
-		std::cout << "Disconnected" << std::endl;
-	});
-	
-	int port = 4567;
-	
-	if (h.listen(port)) {
-		std::cout << "Listening to port " << port << std::endl;
 	}
 	else {
-		std::cerr << "Failed to listen to port" << std::endl;
-		return -1;
+		
+		uWS::Hub h;
+		
+		//Set up parameters here
+		double delta_t = 0.1; // Time elapsed between measurements [sec]
+		double sensor_range = 50; // Sensor range [m]
+		double sigma_pos [3] = {0.3, 0.3, 0.01}; // GPS measurement uncertainty [x [m], y [m], theta [rad]]
+		double sigma_landmark [2] = {0.3, 0.3}; // Landmark measurement uncertainty [x [m], y [m]]
+		
+		// Read map data
+		Map map;
+		if (!read_map_data("../data/map_data.txt", map)) {
+			
+			cout << "Error: Could not open map file" << endl;
+			
+			return -1;
+			
+		}
+		
+		// Create particle filter
+		ParticleFilter pf;
+		
+		h.onMessage([&pf,&map,&delta_t,&sensor_range,&sigma_pos,&sigma_landmark](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+			// "42" at the start of the message means there's a websocket message event.
+			// The 4 signifies a websocket message
+			// The 2 signifies a websocket event
+			
+			if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+				
+				auto s = hasData(std::string(data));
+				
+				if (s != "") {
+					
+					auto j = json::parse(s);
+					std::string event = j[0].get<std::string>();
+					
+					if (event == "telemetry") {
+						// j[1] is the data JSON object
+						
+						// redirect standard output to file if necessary
+						if (bFILEOUTPUT) {
+							cout.rdbuf(out.rdbuf());
+						}
+						
+						// display message if required
+						if (bDISPLAY) {
+							cout << "= = = = = = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+							cout << "MAIN: onMessage - Start" << endl;
+						}
+						
+						if (!pf.initialized()) {
+							// Sense noisy position data from the simulator
+							
+							double sense_x = std::stod(j[1]["sense_x"].get<std::string>());
+							double sense_y = std::stod(j[1]["sense_y"].get<std::string>());
+							double sense_theta = std::stod(j[1]["sense_theta"].get<std::string>());
+							
+							pf.init(sense_x, sense_y, sense_theta, sigma_pos);
+							
+						}
+						else {
+							// Predict the vehicle's next state from previous (noiseless control) data.
+							
+							double previous_velocity = std::stod(j[1]["previous_velocity"].get<std::string>());
+							double previous_yawrate = std::stod(j[1]["previous_yawrate"].get<std::string>());
+							
+							pf.prediction(delta_t, sigma_pos, previous_velocity, previous_yawrate);
+							
+						}
+						
+						// receive noisy observation data from the simulator
+						// sense_observations in JSON format [{obs_x,obs_y},{obs_x,obs_y},...{obs_x,obs_y}]
+						vector<LandmarkObs> noisy_observations;
+						string sense_observations_x = j[1]["sense_observations_x"];
+						string sense_observations_y = j[1]["sense_observations_y"];
+						
+						std::vector<float> x_sense;
+						std::istringstream iss_x(sense_observations_x);
+						std::copy(std::istream_iterator<float>(iss_x),
+						std::istream_iterator<float>(),
+						std::back_inserter(x_sense));
+						
+						std::vector<float> y_sense;
+						std::istringstream iss_y(sense_observations_y);
+						std::copy(std::istream_iterator<float>(iss_y),
+						std::istream_iterator<float>(),
+						std::back_inserter(y_sense));
+						
+						for(unsigned int i = 0; i < x_sense.size(); i++) {
+							
+							LandmarkObs obs;
+							obs.x = x_sense[i];
+							obs.y = y_sense[i];
+							noisy_observations.push_back(obs);
+							
+						}
+						
+						// Update the weights and resample
+						pf.updateWeights(sensor_range, sigma_landmark, noisy_observations, map);
+						pf.resample();
+						
+						// Calculate and output the average weighted error of the particle filter over all time steps so far.
+						vector<Particle> particles = pf.particles;
+						int num_particles = particles.size();
+						double highest_weight = -1.0;
+						Particle best_particle;
+						double weight_sum = 0.0;
+						for (int i = 0; i < num_particles; ++i) {
+							
+							if (particles[i].weight > highest_weight) {
+								
+								highest_weight = particles[i].weight;
+								best_particle = particles[i];
+								
+							}
+							
+							weight_sum += particles[i].weight;
+							
+						}
+						cout << "highest w " << highest_weight << endl;
+						cout << "average w " << weight_sum/num_particles << endl;
+						
+						json msgJson;
+						msgJson["best_particle_x"] = best_particle.x;
+						msgJson["best_particle_y"] = best_particle.y;
+						msgJson["best_particle_theta"] = best_particle.theta;
+						
+						//Optional message data used for debugging particle's sensing and associations
+						msgJson["best_particle_associations"] = pf.getAssociations(best_particle);
+						msgJson["best_particle_sense_x"] = pf.getSenseX(best_particle);
+						msgJson["best_particle_sense_y"] = pf.getSenseY(best_particle);
+						
+						auto msg = "42[\"best_particle\"," + msgJson.dump() + "]";
+						// std::cout << msg << std::endl;
+						ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+						
+						// display message if required
+						if (bDISPLAY) {
+							cout << "  msg: " << endl << msg << endl;
+							cout << "--- MAIN: onMessage - End" << endl;
+							cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+						}
+						
+						// set standard output to screen if necessary
+						if (bFILEOUTPUT) {
+							cout.rdbuf(coutbuf);
+						}
+						
+					}
+					
+				} else {
+					
+					std::string msg = "42[\"manual\",{}]";
+					ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+					
+				}
+				
+			}
+			
+		});
+		
+		// We don't need this since we're not using HTTP but if it's removed the program
+		// doesn't compile :-(
+		h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
+			
+			const std::string s = "<h1>Hello world!</h1>";
+			
+			if (req.getUrl().valueLength == 1) {
+				
+				res->end(s.data(), s.length());
+				
+			}
+			else {
+				
+				// i guess this should be done more gracefully?
+				res->end(nullptr, 0);
+				
+			}
+			
+		});
+		
+		h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+			std::cout << "Connected!!!" << std::endl;
+		});
+		
+		h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
+			ws.close();
+			std::cout << "Disconnected" << std::endl;
+		});
+		
+		int port = 4567;
+		
+		if (h.listen(port)) {
+			std::cout << "Listening to port " << port << std::endl;
+		}
+		else {
+			std::cerr << "Failed to listen to port" << std::endl;
+			return -1;
+		}
+		
+		h.run();
+		
 	}
-	
-	h.run();
 	
 }
