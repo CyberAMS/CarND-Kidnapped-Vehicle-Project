a70b55aece6c1973e0f3e0e71674dfa0498a98c0
strobel <andre.strobel@cyberams.de>
2019-01-02 16:16:26 -0800

    Debug output

--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,8 @@ std::string hasData(std::string s) {
 }
 
 // define constants
-const bool bFILEOUTPUT = false;
-const bool bDISPLAY = false;
+const bool bFILEOUTPUT = true;
+const bool bDISPLAY = true;
 const bool bTEST = false;
 
 // define file for redirecting standard output and append
